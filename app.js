9e418205c59a6f269ad9b5a0d0742071e6af7fe5
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-05-28 18:59:23 -0400

    subien el archivo

---
+++ app.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+
+    function ordenandoComida(option) {
+
+        if (option == 1) {
+            alert('No tenemos ceviche, pero hay tacos al pastor');
+        } else if (option == 2){
+            alert('No tenemos tacos al pastor, pero hay empanada chilena');
+        }else if(option == 3){
+            alert('No tenemos empanada chilena, pero hay hamburguesa');
+        } else if(option == 4){
+            alert('Buena elecciòn');
+        } else if(option == 5){
+            alert('Hasta pronto');
+        }else{
+        alert('Elige elgo mas del menu');
+          }
+      }
+    
+
+}
